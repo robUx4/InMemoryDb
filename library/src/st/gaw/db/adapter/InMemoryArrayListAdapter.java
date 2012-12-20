094a9052000d54cf337dae5b5d0b48a31a2aea6a
Steve Lhomme <robux4@gmail.com>
2012-12-20 11:19:03 +0100

    add simple adapters (similar to SimpleAdapter) for InMemoryDbArrayList<> and a filtered version of data from a InMemoryDbArrayList<>

---
+++ library/src/st/gaw/db/adapter/InMemoryArrayListAdapter.java
@@ -0,0 +1,61 @@
+package st.gaw.db.adapter;
+
+import st.gaw.db.InMemoryDbArrayList;
+import st.gaw.db.InMemoryDbHelper;
+import st.gaw.db.InMemoryDbListener;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class InMemoryArrayListAdapter<E> extends BaseAdapter implements InMemoryDbListener<E> {
+
+	private final InMemoryDbArrayList<E> mArray;
+	private final LayoutInflater mInflater;
+	private final int layoutId;
+	
+	public InMemoryArrayListAdapter(Context context, InMemoryDbArrayList<E> array, int layoutResourceId) {
+		this.mArray = array;
+		this.mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		this.layoutId = layoutResourceId;
+		mArray.addListener(this);
+	}
+
+	@Override
+	public int getCount() {
+		return mArray.getCount();
+	}
+
+	@Override
+	public Object getItem(int position) {
+		return mArray.get(position);
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+	
+	@Override
+	public long getItemId(int position) {
+		return mArray.get(position).hashCode();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView==null)
+			convertView = mInflater.inflate(layoutId, parent, false);
+
+		TextView vt = (TextView) convertView.findViewById(android.R.id.text1);
+		vt.setText(mArray.get(position).toString());
+		
+		return convertView;
+	}
+
+	@Override
+	public void onMemoryDbChanged(InMemoryDbHelper<E> db) {
+		notifyDataSetChanged();
+	}
+}
---
+++ library/src/st/gaw/db/adapter/InMemoryFilteredListAdapter.java
@@ -0,0 +1,73 @@
+package st.gaw.db.adapter;
+
+import java.util.List;
+
+import st.gaw.db.InMemoryDbArrayList;
+import st.gaw.db.InMemoryDbHelper;
+import st.gaw.db.InMemoryDbList;
+import st.gaw.db.InMemoryDbListener;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class InMemoryFilteredListAdapter<E> extends BaseAdapter implements InMemoryDbListener<E> {
+
+	public interface DbFilter<E> {
+		List<E> getFilteredData(InMemoryDbList<E,?> source);
+	}
+	
+	private final InMemoryDbArrayList<E> mArray;
+	private final LayoutInflater mInflater;
+	private final int layoutId;
+	private final DbFilter<E> filter;
+	private List<E> mData;
+	
+	public InMemoryFilteredListAdapter(Context context, InMemoryDbArrayList<E> array, int layoutResourceId, DbFilter<E> filter) {
+		this.mArray = array;
+		this.mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		this.layoutId = layoutResourceId;
+		this.filter = filter;
+		mData = filter.getFilteredData(mArray);
+		mArray.addListener(this);
+	}
+
+	@Override
+	public int getCount() {
+		return mData.size();
+	}
+
+	@Override
+	public E getItem(int position) {
+		return mData.get(position);
+	}
+
+	@Override
+	public boolean hasStableIds() {
+		return true;
+	}
+	
+	@Override
+	public long getItemId(int position) {
+		return mData.get(position).hashCode();
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (convertView==null)
+			convertView = mInflater.inflate(layoutId, parent, false);
+
+		TextView vt = (TextView) convertView.findViewById(android.R.id.text1);
+		vt.setText(mData.get(position).toString());
+		
+		return convertView;
+	}
+
+	@Override
+	public void onMemoryDbChanged(InMemoryDbHelper<E> db) {
+		mData = filter.getFilteredData(mArray);
+		notifyDataSetChanged();
+	}
+}
