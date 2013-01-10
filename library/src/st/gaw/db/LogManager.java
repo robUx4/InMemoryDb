3b928d66997ef9a222bed9dfb434ca2f11320053
Steve Lhomme <robux4@gmail.com>
2013-01-10 14:34:10 +0100

    better version of LogManager that fits more scenarii

--- library/src/st/gaw/db/LogManager.java
+++ library/src/st/gaw/db/LogManager.java
@@ -3,11 +3,15 @@ package st.gaw.db;
 /**
  * class that holds the {@link Logger} for this library
  */
-public class LogManager {
+public final class LogManager {
 
 	static Logger logger = new LoggerDefault();
 	
 	public static void setLogger(Logger newLogger) {
 		logger = newLogger;
+	}
+	
+	public static Logger getLogger() {
+		return logger;
 	}
 }
